48e626453d654431e147a43e5ba2c982ebd4d008
Nidsonr1 <nidsoncampos@hotmail.com>
2021-10-07 11:40:11 -0300

    Adicionando o multer para upload de categorias e criando o useCase das importacoes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/categories/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/categories/listCategories';
-
+import { importCategoryController } from '../modules/cars/useCases/categories/importCategory';
 
 const categoriesRoute = Router();
-
+const upload = multer({ dest: './tmp' });
 
 categoriesRoute.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -13,6 +14,10 @@ categoriesRoute.post('/', (request, response) => {
 
 categoriesRoute.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoute.post('/imports', upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoute }
