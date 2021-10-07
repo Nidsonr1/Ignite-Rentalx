35e2ef21efea0fc8c71d92a8726033217e5665e6
Nidsonr1 <nidsoncampos@hotmail.com>
2021-10-07 15:08:25 -0300

    Lendo o Arquivo csv

--- src/modules/cars/useCases/categories/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/categories/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,18 @@
-
-
+import csvParse from "csv-parse"
+import fs from "fs";
 
 class ImportCategoryUseCase {
 
-  execute(file: any) {
-    console.log(file)
+  execute(file: Express.Multer.File) {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", (line) => {
+      console.log(line);
+    })
   }
 }
 
