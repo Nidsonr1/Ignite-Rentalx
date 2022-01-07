c1fa6a7a904794a1ec762cc1cc41cd261bd50f6c
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-06 21:04:38 -0300

    Refatorando Specification

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import "./database";
+import "./shared/containers"
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 
--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -1,8 +1,16 @@
 import { container } from 'tsyringe';
+
 import { ICategoryRepository } from '../../modules/cars/repositories/ICategoryRepository';
 import { CategoryRepository } from '../../modules/cars/repositories/implementations/CategoryRepository';
+import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationRepository';
+import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationRepository';
 
 container.registerSingleton<ICategoryRepository>(
   "CategoryRepository",
   CategoryRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
 );
