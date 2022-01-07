19ea429c5c23e0d9e5944796d18cdf38029f96e1
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-07 16:25:28 -0300

    Criptografando Senha

--- src/modules/accounts/useCase/createUsers/CreateUsersUseCase.ts
+++ src/modules/accounts/useCase/createUsers/CreateUsersUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcrypt'
 
 import { ICreateUsersDTO } from '../../repositories/IUsersRepository';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
@@ -14,12 +15,14 @@ class CreateUsersUseCase {
   async execute({name, email, password, driver_license}: ICreateUsersDTO) {
     const userAlreadyExist = await this.usersRepository.findByEmail(email);
 
-    if(userAlreadyExist) throw new Error("Email already registred")
+    if(userAlreadyExist) throw new Error("Email already registred");
+
+    const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license
     });
   }
